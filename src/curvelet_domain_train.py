8a8be29d42e604af07ca1bd76f581eb25ecc43ff
Átila Saraiva <atilasaraiva@gmail.com>
2022-08-17 15:29:31 -0300

    merged space domain filter train domain class into a single file

--- src/curvelet_domain_train.py
+++ src/curvelet_domain_train.py
@@ -7,15 +7,16 @@ import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from sklearn.preprocessing import RobustScaler, MaxAbsScaler
+from typing import Callable
+from trainUnetClass import CurveletFilter, make_curv_transform
 import os
 
-epochs = 30
-scaler = RobustScaler()
 
 class TrainSetup(pl.LightningModule):
 
     def __init__(self,
                  model: torch.nn.Module,
+                 transform: Callable,
                  train_loader: torch.utils.data.DataLoader,
                  test_loader: torch.utils.data.DataLoader = None,
                  val_loader: torch.utils.data.DataLoader = None,
@@ -26,28 +27,44 @@ class TrainSetup(pl.LightningModule):
         self.test_loader = test_loader or self.train_loader
         self.val_loader = val_loader or self.test_loader
         self.learning_rate = learning_rate
-
+        self.transform = transform
+        
+ 
     def train_dataloader(self):
         return self.train_loader
 
     def val_dataloader(self):
         return self.val_loader
-
+ 
     def test_dataloader(self):
         return self.test_loader
-
+ 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
-
+ 
     def training_step(self, batch, batch_idx):
         X, Y = batch
-        Y_pred = self.model(X)
-        loss =  F.mse_loss(Y, Y_pred)
+        residue = torch.tensor(0, dtype=torch.float32)
+        for x, y in zip(X, Y):
+            xc = tuple([torch.from_numpy(i) for i in self.transform(x[0])])
+            yc = tuple([torch.from_numpy(i) for i in self.transform(y[0])])
+            yc_pred = self.model(xc)
+            for i in range(len(yc_pred)):
+                residue += F.mse_loss(yc[i], yc_pred[i])
+        loss = residue / len(X)
         return loss
-
+ 
     def validation_step(self, batch, batch_idx):
-        loss = self.training_step(batch, batch_idx)
+        X, Y = batch
+        residue = torch.tensor(0, dtype=torch.float32)
+        for x, y in zip(X, Y):
+            xc = tuple([torch.from_numpy(i) for i in self.transform(x[0])])
+            yc = tuple([torch.from_numpy(i) for i in self.transform(y[0])])
+            yc_pred = self.model(xc)
+            for i in range(len(yc_pred)):
+                residue += F.mse_loss(yc[i], yc_pred[i])
+        loss = residue / len(X)
         metrics = {"loss": loss}
         metrics = {f"val_{k}": v for k, v in metrics.items()}
         self.log_dict(metrics)  #, sync_dist=True))
@@ -56,25 +73,20 @@ class TrainSetup(pl.LightningModule):
 dataFolder = os.environ["DATADIR"]
 rtm_file = h5py.File(dataFolder + "rtm.h5")
 rtm_dset = rtm_file["m"]
-scaler_mig = scaler.fit(rtm_dset)
-rtm_norm = scaler_mig.transform(rtm_dset)[20:,:]
+scaler_mig = MaxAbsScaler().fit(rtm_dset)
+rtm_norm = scaler_mig.transform(rtm_dset)
 
 rtmRemig_file = h5py.File(dataFolder + "rtm_remig.h5")
 rtmRemig_dset = rtmRemig_file["m"]
-scaler_remig = scaler.fit(rtmRemig_dset)
-rtmRemig_norm = scaler_remig.transform(rtmRemig_dset)[20:,:]
+scaler_remig = MaxAbsScaler().fit(rtmRemig_dset)
+rtmRemig_norm = scaler_remig.transform(rtmRemig_dset)
 
-# plt.imshow(rtmRemig_norm)
-# plt.savefig("rtmremig.pdf")
-# plt.show()
-# plt.imshow(rtm_norm)
-# plt.savefig("rtm.pdf")
-# plt.show()
+X, Y = extract_patches(rtmRemig_norm, rtm_norm, patch_num=600, patch_size=32)
 
-X, Y = extract_patches(rtmRemig_norm, rtm_norm, patch_num=150, patch_size=32)
+X_train, X_test = X[:500,:,:,:], X[500:,:,:,:]
+Y_train, Y_test = Y[:500,:,:,:], Y[500:,:,:,:]
 
-X_train, X_test = X[:130,:,:,:], X[130:,:,:,:]
-Y_train, Y_test = Y[:130,:,:,:], Y[130:,:,:,:]
+curv_fwd, curv_inv, curv_shapes = make_curv_transform(X[0,0])
 
 train_dataset = torch.utils.data.TensorDataset(X_train, Y_train)
 test_dataset = torch.utils.data.TensorDataset(X_test, Y_test)
@@ -82,16 +94,18 @@ test_dataset = torch.utils.data.TensorDataset(X_test, Y_test)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=5, num_workers=20)  #, prefetch_factor=3, num_workers=3)
 test_loader = torch.utils.data.DataLoader(train_dataset, batch_size=5, num_workers=20)  #, prefetch_factor=3, num_workers=3)
 
-model = UNet(ndim=2, in_channels=1, out_channels=1, norm=False)
+#model = UNet(ndim=2, in_channels=1, out_channels=1, norm=False)
+model =  CurveletFilter(curv_shapes)
 
 train_setup = TrainSetup(
     model,
+    transform=curv_fwd,
     train_loader=train_loader,
     test_loader=test_loader,
     learning_rate=0.005,
 )
 
-trainer = pl.Trainer(max_epochs=epochs, limit_train_batches=50)
+trainer = pl.Trainer(max_epochs=10, limit_train_batches=50)
 trainer.fit(train_setup)
 
 modeldir = os.environ['MODELDIR']
