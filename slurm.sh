345c0c9cf16e379680b86b80c1e3bfabd4a98a71
Átila Saraiva <atilasaraiva@gmail.com>
2022-08-16 23:24:45 -0300

    added slurm script

---
+++ slurm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#SBATCH --job-name=nn                          # Job name
+#SBATCH --nodes=1                              # Run all processes on 2 nodes
+#SBATCH --partition=gpulongb                   # Partition OGBON
+#SBATCH --output=out_%j.log                    # Standard output and error log
+#SBATCH --ntasks-per-node=1                    # 1 job per node
+#SBATCH --account=cenpes-lde                   # Account of the group
+
+NIXPKGS_ALLOW_UNFREE=1 nix-portable nix-shell --command "cd src && make"
