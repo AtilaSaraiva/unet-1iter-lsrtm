53139024b03b235fb9110ebc06cda8d24841be4e
Átila Saraiva <atilasaraiva@gmail.com>
2022-08-18 00:40:42 -0300

    started working on the inference

---
+++ src/curvelet_domain_inference.py
@@ -0,0 +1,50 @@
+import os
+import h5py
+import torch
+from tiler import Tiler, Merger
+from sklearn.preprocessing import RobustScaler, MaxAbsScaler
+from trainUnetClass import CurveletFilter, make_curv_transform
+from unet import UNet
+from matplotlib import pyplot as plt
+
+scaler = RobustScaler()
+
+dataFolder = os.environ["DATADIR"]
+
+rtm_file = h5py.File(dataFolder + "rtm.h5")
+rtm_dset = rtm_file["m"]
+scaler_mig = scaler.fit(rtm_dset)
+rtm_norm = scaler_mig.transform(rtm_dset)
+original_shape = rtm_norm.shape
+rtm_norm = rtm_norm.reshape((1,*original_shape))
+
+tiler = Tiler(data_shape=rtm_norm.shape,
+      tile_shape=(1, 40, 60),
+      overlap=(0,30,50),
+      channel_dimension=0)
+
+tiles = tiler.get_all_tiles(rtm_norm)
+tiles = torch.from_numpy(tiles)
+
+modeldir = os.environ['MODELDIR']
+# model = UNet(ndim=2, in_channels=1, out_channels=1, norm=False)
+model =  CurveletFilter(curv_shapes)
+model.load_state_dict(torch.load(modeldir + "curveletUnet.pt"))
+
+with torch.no_grad():
+    filtered_tiles = model(tiles)
+
+    merger = Merger(tiler)
+    merger.add_batch(0, filtered_tiles.shape[0], filtered_tiles.numpy())
+
+    normalized_filtered_image = merger.merge(unpad=True).reshape(original_shape)
+
+filtered_image = scaler_mig.inverse_transform(normalized_filtered_image)
+
+fig, ax = plt.subplots(2)
+ax[0].imshow(rtm_dset, cmap="seismic")
+ax[1].imshow(filtered_image, cmap="seismic")
+plt.show()
+
+with h5py.File(dataFolder + "filtered_space_domain_image.h5", "w") as f:
+    f.create_dataset('m', data=filtered_image)
