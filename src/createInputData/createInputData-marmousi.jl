646b546f57c1badb389d4e29ea964e57dd8e996f
Átila Saraiva <atilasaraiva@gmail.com>
2022-10-20 22:58:40 -0600

    finally taking shape the marmousi generation code

--- src/createInputData/createInputData-marmousi.jl
+++ src/createInputData/createInputData-marmousi.jl
@@ -1,11 +1,15 @@
 using JUDI, SegyIO, HDF5, PyPlot, Random
 
+modelName = "marmousi"
+dataFolder = "../../data/"
+mkpath(dataFolder)
 
 # Load migration velocity model
 if ~isfile("$(JUDI.JUDI_DATA)/marmousi_model.h5")
     ftp_data("ftp://slim.gatech.edu/data/SoftwareRelease/Imaging.jl/2DLSRTM/marmousi_model.h5")
 end
 n, d, o, m0 = read(h5open("$(JUDI.JUDI_DATA)/marmousi_model.h5", "r"), "n", "d", "o", "m0")
+print(n)
 
 # Set up model structure
 model0 = Model((n[1], n[2]), (d[1], d[2]), (o[1], o[2]), m0)
@@ -50,11 +54,30 @@ Ml = judiMarineTopmute2D(30, dinv.geometry)
 # print(d_lin.data[1])
 
 rtm = adjoint(Ml*Jinv*Mr)*dinv
+d_calc = Ml*Jinv*Mr*rtm
+rtm_remig = adjoint(Ml*Jinv*Mr)*d_calc
 
-# #' Plot the shot record
-# fig = figure()
-# imshow(d_lin.data[1], cmap="PuOr", aspect="auto")
-# xlabel("Receiver position (m)")
-# ylabel("Time (s)")
-# title("Synthetic data")
-# display(fig)
+rtm = reshape(rtm, (n[1], n[2]))
+rtm_remig = reshape(rtm_remig, (n[1], n[2]))
+
+# Save migrated image
+h5open(dataFolder*"rtm_$(modelName).h5", "w") do file
+    write(file, "m", rtm)
+    write(file, "d", collect(d))
+end
+
+# Save remigrated image
+h5open(dataFolder*"rtm_remig_$(modelName).h5", "w") do file
+    write(file, "m", rtm_remig)
+    write(file, "d", collect(d))
+end
+
+#' And the RTM image
+fig = figure()
+imshow(rtm', cmap="Greys", extent=[0, (n[1]-1)*d[1], (n[2]-1)*d[2], 0 ], aspect="auto")
+imshow(rtm_remig', cmap="Greys", extent=[0, (n[1]-1)*d[1], (n[2]-1)*d[2], 0 ], aspect="auto")
+xlabel("Lateral position(m)")
+ylabel("Depth (m)")
+title("RTM image")
+savefig("rtm.png", dpi=300)
+display(fig)
