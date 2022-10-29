689932546ee34e80861a9c5703d656b0f509652d
Átila Saraiva <atilasaraiva@gmail.com>
2022-10-29 14:28:25 -0600

    adding downscaling into the createInputData-sigsbee

--- src/createInputData/createInputData-sigsbee.jl
+++ src/createInputData/createInputData-sigsbee.jl
@@ -10,17 +10,19 @@ modelName = "sigsbee2A"
 mkpath(dataFolder)
 data_name="sigsbee2A_marine"
 
-using JUDI, PyPlot, JLD, SegyIO
+using JUDI, PyPlot, JLD, SegyIO, HDF5
 
 # Load Sigsbee model
-if !isfile("sigsbee2A_model.jld")
+if !isfile("$(dataFolder)vel_$(modelName)-down.h5")
     run(`wget ftp://slim.gatech.edu/data/SoftwareRelease/Imaging.jl/CompressiveLSRTM/sigsbee2A_model.jld`)
+    include("create-sigsbee-hdf5.jl")
+    run(`python downscale.py infile="$(dataFolder)vel_sigsbee2A.h5" fx=0.5 fy=1.`)
 end
-M = load("sigsbee2A_model.jld")
+n, d, o, m0, dm = read(h5open("$(dataFolder)vel_$(modelName)-down.h5", "r"), "n", "d", "o", "m0", "dm")
 
 # Setup model structure
-model0 = Model(M["n"], M["d"], M["o"], M["m0"])
-dm = vec(M["dm"])
+model0 = Model((n[1], n[2]), (d[1], d[2]), (o[1], o[2]), m0)
+dm = vec(dm)
 
 # Load data
 container = segy_scan(dataFolder, data_name, ["GroupX","GroupY","RecGroupElevation","SourceSurfaceElevation","dt"])
@@ -45,7 +47,9 @@ Ps = judiProjection(src_geometry)
 J = judiJacobian(Pr*F0*Ps', q)
 
 #' set up number of iterations
-indsrc = Vector(1:Int(floor(q.nsrc/3)))*3
+# indsrc = Vector(1:Int(floor(q.nsrc/3)))*3
+nsrc = 5 * parse(Int, get(ENV, "NITER", "$(q.nsrc ÷ 5)"))
+indsrc = randperm(q.nsrc)[1:nsrc]
 dinv = d_lin[indsrc]
 Jinv = J[indsrc]
 
@@ -55,27 +59,27 @@ Tm = judiTopmute(model0.n, idx_wb, 10)  # Mute water column
 S = judiDepthScaling(model0)
 Mr = S*Tm
 
-rtm = adjoint(Jinv*Mr)*d_inv
+rtm = adjoint(Jinv*Mr)*dinv
 d_calc = Jinv*Mr*rtm
 rtm_remig = adjoint(Jinv*Mr)*d_calc
 
-rtm = reshape(rtm, M["n"])
-rtm_remig = reshape(rtm_remig, M["n"])
+rtm = reshape(rtm, (n[1], n[2]))
+rtm_remig = reshape(rtm_remig, (n[1], n[2]))
 
 # Save migrated image
 h5open(dataFolder*"rtm_$(modelName).h5", "w") do file
     write(file, "m", rtm)
-    write(file, "d", collect(M["d"]))
+    write(file, "d", collect((d[1], d[2])))
 end
 
 # Save remigrated image
 h5open(dataFolder*"rtm_remig_$(modelName).h5", "w") do file
     write(file, "m", rtm_remig)
-    write(file, "d", collect(M["d"]))
+    write(file, "d", collect((d[1], d[2])))
 end
 
 # Save the velocity model image
 h5open(dataFolder*"vel_$(modelName).h5", "w") do file
-    write(file, "m0", M["m0"])
-    write(file, "d", collect(M["d"]))
+    write(file, "m0", m0)
+    write(file, "d", collect((d[1], d[2])))
 end
