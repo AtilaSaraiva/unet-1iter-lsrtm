1c1e7bfdf1e0275adab48011079c603956aae52b
Átila Saraiva <atilasaraiva@gmail.com>
2022-10-29 15:57:00 -0600

    adding slurm scripts for data creation and lsrtm

---
+++ src/createInputData/slurm-lsrtmsigsbee.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#SBATCH --job-name=nn                          # Job name
+#SBATCH --nodes=1                              # Run all processes on 2 nodes
+#SBATCH --partition=gpulongb                   # Partition OGBON
+#SBATCH --output=out_%j.log                    # Standard output and error log
+#SBATCH --ntasks-per-node=1                    # 1 job per node
+#SBATCH --account=cenpes-lde                   # Account of the group
+
+source "/opt/share/anaconda3/2022.05/etc/profile.d/conda.sh"
+conda activate judi
+
+srun julia --project=. lsrtm-sigsbee.jl
