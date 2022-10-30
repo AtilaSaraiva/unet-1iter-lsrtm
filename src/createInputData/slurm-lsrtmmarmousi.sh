af537cbfce552c2cccbb6bd63b3abbb3291c3b53
Átila Saraiva <atilasaraiva@gmail.com>
2022-10-29 18:03:48 -0600

    added names to slurm jobs

--- src/createInputData/slurm-lsrtmmarmousi.sh
+++ src/createInputData/slurm-lsrtmmarmousi.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#SBATCH --job-name=nn                          # Job name
+#SBATCH --job-name=lsrtm-marm                          # Job name
 #SBATCH --nodes=1                              # Run all processes on 2 nodes
-#SBATCH --partition=gpulongb                   # Partition OGBON
+#SBATCH --partition=cpulongb                   # Partition OGBON
 #SBATCH --output=out_%j.log                    # Standard output and error log
 #SBATCH --ntasks-per-node=1                    # 1 job per node
 #SBATCH --account=cenpes-lde                   # Account of the group
